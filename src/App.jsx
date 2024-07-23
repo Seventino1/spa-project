23e9764b580576005666f25428a9922dd2b867bd
Volkov Ilia <joker_077@mail.ru>
2024-07-23 17:03:48 +0500

    Header + Filter + Search

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Controls } from "./components/Controls";
+import { Header } from "./components/Header1";
+import { Main } from "./components/Main";
+
+function App() {
+
+  
+  return (
+    <>
+      <Header />
+      <Main>
+        <Controls />
+      </Main>
+    </>
+  );
+}
+
+export default App;
